8b52907a1d1e3f2ec9d28c36eb398fdc28ee34e2
Ella Milliken Detro <emillikendetro@gmail.com>
2018-12-16 14:21:18 -0500

    final update

--- asist_page_scrape.py
+++ asist_page_scrape.py
@@ -26,15 +26,16 @@ for url in links:
     for catagory in job_detail:
         #find 'label' in html
         label = catagory.find("label")
+
         #find 'span' in html
         info = catagory.find("span")
         #organize label and info into dictionary
-        asist_job[label.text] = info.text
+
+        asist_job[label.text.strip(": ")] = info.text.strip()
 
     #add finished dictionary entries (asist_job) to list (asist_job_links)
     if asist_job not in asist_job_list:
         asist_job_list.append(asist_job)
-
-        json.dump(asist_job_list, open('asist_job_list.json','w'), indent=2)
+    json.dump(asist_job_list, open('asist_job_list.json','w'), indent=2)
 
     time.sleep(1)
--- saa_page_scrape.py
+++ saa_page_scrape.py
@@ -17,23 +17,45 @@ for url in links:
     page_html = job_page.text
     #bring in beautiful soup to read html
     all_html = BeautifulSoup(page_html, "html.parser")
-    #go to job title in html
-    job_title = all_html.find_all("h1", attrs = {"class":"bti-jd-title"})
-    #go to employer name in html
-    job_inst = all_html.find_all("h2", attrs = {"class" : "bti-jd-employer-title"})
-    #go to relevant part of html (job detail box)
-    #job_detail = all_html.find_all("div", attrs = {"class":"bti-jd-details-container"})
-    #loop through each item in the list
-    #for catagory in job_detail:
-        #find date 'info' in html
-        #date_info = catagory.find("bti-jd-detail-text")
-        #find 'location' in html
-        #location = catagory.find("span")
-        #open dictionary that will go in existing list
-    saa_job = {"Title" : job_title.text}
-        #, "Posted" : date_info.text, "Location" : location.text
-    saa_job_list.append(saa_job)
 
+    job_title = all_html.find("h1", attrs = "bti-jd-title")
+    #assign variable to text of job title
+    position = job_title["title"]
+
+    job_company = all_html.find("h2", attrs = "bti-jd-employer-title")
+    company = job_company.text.strip()
+
+    date_posted = all_html.find("div", attrs = "bti-jd-detail-text")
+    date = date_posted.text
+
+    job_location = all_html.find("div", attrs = "bti-jd-details-action-container")
+    location = job_location.find("span").text
+
+    other_details = all_html.find("div", attrs = "bti-jd-details-other")
+
+    for info in other_details:
+        salary = other_details.text
+        type_exp = other_details.text
+        if type_exp is "Entry Level":
+            entry_level = "Yes"
+        else:
+            entry_level = "No"
+        job_func_setting = other_details.text
+        edu = other_details.text
+
+    saa_job = {
+        "Posted" : date,
+        "Location" : location,
+        "Entry Level" : entry_level,
+        "Company Name" : company,
+        "Position Title" : position,
+        "Job Function/Setting" : job_func_setting,
+        "Job Type/Min Experience" : type_exp,
+        "Min Education" : edu,
+        "Salary" : salary,
+        }
+    saa_job_list.append(saa_job)
+    #print(saa_job_list)
     json.dump(saa_job_list, open('saa_job_list.json','w'), indent=2)
 
-    time.sleep(1)
+time.sleep(1)
